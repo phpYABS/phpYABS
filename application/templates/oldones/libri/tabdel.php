5d358aa0e77d7919daea875fa8463f82401d983f
Davide Bellettini <davide@bellettini.eu>
2022-02-10 22:45:58 +0100

    Fix phpstan level 1

--- application/templates/oldones/libri/tabdel.php
+++ application/templates/oldones/libri/tabdel.php
@@ -3,38 +3,38 @@
   <tr>
     <th width="80">ISBN:</th>
     <td>
-      <?php echo $ISBN; ?>
+      <?php echo $ISBN ?? ''; ?>
     </td>
   </tr>
   <tr>
     <th width="80">Titolo:</th>
     <td>
-      <?php echo $Titolo; ?>
+      <?php echo $Titolo ?? ''; ?>
     </td>
   </tr>
   <tr>
     <th width="80">Autore:</th>
     <td>
-      <?php echo $Autore; ?>
+      <?php echo $Autore ?? ''; ?>
     </td>
   </tr>
   <tr>
     <th width="80">Editore:</th>
     <td>
-      <?php echo $Editore; ?>
+      <?php echo $Editore ?? ''; ?>
     </td>
   </tr>
   <tr>
     <th width="80">Prezzo:</th>
     <td>
-      <?php echo $Prezzo; ?>
+      <?php echo $Prezzo ?? ''; ?>
     </td>
   </tr>
   <tr>
     <th width="80">Valutazione:</th>
-    <td><u><?php echo $Valutazione; ?></u></td>
+    <td><u><?php echo $Valutazione ?? ''; ?></u></td>
   </tr>
 </table>
 </p>
 <div align="center"><a href="<?php echo $_SERVER['PHP_SELF'] . '?Nome=' . $_GET['Nome'] . '&Azione=' . $_GET['Azione'] . '&ISBN=' .
-$ISBN . '&cancella=on'; ?>">Cancella Libro</a>
+        ($ISBN ?? '') . '&cancella=on'; ?>">Cancella Libro</a>
--- application/templates/oldones/libri/tabmod.php
+++ application/templates/oldones/libri/tabmod.php
@@ -2,34 +2,34 @@
 <table width="600" border="1" align="center">
   <tr>
     <th width="80">ISBN:</th>
-    <td><?php echo $ISBN; ?>
-    <input type="hidden" name="ISBN" value="<?php echo $ISBN; ?>">
+    <td><?php echo $ISBN ?? ''; ?>
+    <input type="hidden" name="ISBN" value="<?php echo $ISBN ?? ''; ?>">
     </td>
   </tr>
   <tr>
     <th width="80">Titolo:</th>
-    <td><input type="text" name="Titolo" value="<?php echo $Titolo; ?>" style="width:300px"></td>
+    <td><input type="text" name="Titolo" value="<?php echo $Titolo ?? ''; ?>" style="width:300px"></td>
   </tr>
   <tr>
     <th width="80">Autore:</th>
-    <td><input type="text" name="Autore" value="<?php echo $Autore; ?>" style="width:300px"></td>
+    <td><input type="text" name="Autore" value="<?php echo $Autore ?? ''; ?>" style="width:300px"></td>
   </tr>
   <tr>
     <th width="80">Editore:</th>
-    <td><input type="text" name="Editore" value="<?php echo $Editore; ?>" style="width:300px"></td>
+    <td><input type="text" name="Editore" value="<?php echo $Editore ?? ''; ?>" style="width:300px"></td>
   </tr>
   <tr>
     <th width="80">Prezzo:</th>
-    <td><input type="text" name="Prezzo" value="<?php echo $Prezzo; ?>" style="width:300px"></td>
+    <td><input type="text" name="Prezzo" value="<?php echo $Prezzo ?? ''; ?>" style="width:300px"></td>
   </tr>
   <tr>
     <th width="80">Valutazione:</th>
     <td><select name="Valutazione">
-      <option value="NULL" <?php echo $selnull; ?>>Nessuna</option>
-      <option value="zero" <?php echo $selzero; ?>>Macero</option>
-      <option value="rotmed" <?php echo $selrotmed; ?>>Rottamazione Medie</option>
-      <option value="rotsup" <?php echo $selrotsup; ?>>Rottamazione Superiori</option>
-      <option value="buono" <?php echo $selbuono; ?>>Buono</option>
+      <option value="NULL" <?php echo $selnull ?? ''; ?>>Nessuna</option>
+      <option value="zero" <?php echo $selzero ?? ''; ?>>Macero</option>
+      <option value="rotmed" <?php echo $selrotmed ?? ''; ?>>Rottamazione Medie</option>
+      <option value="rotsup" <?php echo $selrotsup ?? ''; ?>>Rottamazione Superiori</option>
+      <option value="buono" <?php echo $selbuono ?? ''; ?>>Buono</option>
     </select></td>
   </tr>
   <tr>

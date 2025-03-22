847075349a7fb19d84d6996e634550e57a65fa52
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-03-22 22:01:44 +0100

    Extract menu template

--- src/Facade/MainFacade.php
+++ src/Facade/MainFacade.php
@@ -1,15 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace PhpYabs\Facade;
 
 use PhpYabs\Configuration\Configuration;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
 use Slim\Views\Twig;
 
 class MainFacade extends AbstractFacade
 {
-    public function index(Request $request, Response $response): Response
+    public function index(Request $request, Response $response): ResponseInterface
     {
         global $intestazione;
         $view = Twig::fromRequest($request);
@@ -20,40 +23,10 @@ class MainFacade extends AbstractFacade
         ]);
     }
 
-    public function menu(Request $request, Response $response): Response
+    public function menu(Request $request, Response $response): ResponseInterface
     {
-        global $edit;
-        ob_start(); ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<HTML>
-<HEAD>
-<META content="text/html; charset=utf-8" http-equiv=Content-Type>
-<title>phpYabs - menu</title>
-<link href="/css/main.css" rel="stylesheet" type="text/css">
-<base target="main">
-</HEAD>
-<BODY>
-<h1>Menu</h1>
-<p><a href="/purchases/current?Azione=Nuovo">Nuovo Acquisto</a></p>
-<form action="/purchases" method="get" name="acquisto">
-        Recupera acquisto:
-  <input name="purchase_id" type="text" id="purchase_id" maxlength="10" style="width:90px">
-  <input type="submit" value="Ok">
-</form>
-<?php if ($edit) { ?>
-    <p><a href="/books/add">Aggiungi Libro</a></p>
-    <p><a href="/books/edit">Modifica Libro</a></p>
-    <p><a href="/books/delete">Cancella Libro</a></p>
-    <?php } ?>
-<p><a href="/purchases">Elenco Acquisti</a></p>
-<p><a href="/destinations">Destinazioni</a></p>
-<p><a href="/stats">Statistiche</a></p>
-<p><a href="/books" target="_blank">Elenco Libri</a></p>
-</BODY>
-</HTML>
-    <?php
-        $response->getBody()->write((string) ob_get_clean());
+        $view = Twig::fromRequest($request);
 
-        return $response;
+        return $view->render($response, 'menu.twig');
     }
 }

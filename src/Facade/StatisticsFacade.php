a32722a3c596dfb861d48f5ca6992f64c9871443
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-03-23 00:20:47 +0100

    Fix phpstan error

--- src/Facade/StatisticsFacade.php
+++ src/Facade/StatisticsFacade.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace PhpYabs\Facade;
 
+use Psr\Http\Message\ResponseInterface;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 use Slim\Views\Twig;
 
 class StatisticsFacade extends AbstractFacade
 {
-    public function index(Request $request, Response $response): Response
+    public function index(Request $request, Response $response): ResponseInterface
     {
         $dbal = $this->getDoctrineConnection();
 

7c0ce9f1f79ca5f403046573e64cc4dc37ed67be
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-03-25 13:28:33 +0100

    Fix Form Type

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace PhpYabs\Form;
 
 use PhpYabs\Entity\Book;
-use PhpYabs\Entity\BuybackRate;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,10 +19,7 @@ class BookType extends AbstractType
             ->add('author')
             ->add('publisher')
             ->add('price')
-            ->add('buybackRate', EntityType::class, [
-                'class' => BuybackRate::class,
-                'choice_label' => 'id',
-            ])
+            ->add('rate')
         ;
     }
 

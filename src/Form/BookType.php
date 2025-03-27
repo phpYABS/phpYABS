65729353750dcd77f968e1daa7a1eb18145688ee
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-03-27 20:23:31 +0100

    Use translations in form

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -18,16 +18,17 @@ class BookType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('isbn')
-            ->add('title')
-            ->add('author')
-            ->add('publisher')
-            ->add('price', MoneyType::class)
+            ->add('isbn', options: ['label' => 'book.fields.isbn'])
+            ->add('title', options: ['label' => 'book.fields.title'])
+            ->add('author', options: ['label' => 'book.fields.author'])
+            ->add('publisher', options: ['label' => 'book.fields.publisher'])
+            ->add('price', MoneyType::class, options: ['label' => 'book.fields.price'])
             ->add('rate', EnumType::class, [
                 'class' => Rate::class,
                 'choice_label' => 'name',
+                'label' => 'book.fields.rating',
             ])
-            ->add('save', SubmitType::class, ['label' => 'Aggiungi'])
+            ->add('save', SubmitType::class, ['label' => 'book.actions.add'])
         ;
     }
 

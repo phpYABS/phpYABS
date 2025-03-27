d3b034902c400ab52b4acc5c59a3566fff7031ee
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-03-27 21:58:16 +0100

    Remove file header

--- src/DB/ActiveRecord.php
+++ src/DB/ActiveRecord.php
@@ -2,35 +2,10 @@
 
 declare(strict_types=1);
 
-// vim: set shiftwidth=4 tabstop=4 expandtab cindent :
-
 namespace PhpYabs\DB;
 
 use Doctrine\DBAL\Connection;
 
-/**
- * phpYABS - Web-based book management
- * Copyright (C) 2003-2012 Davide Bellettini.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @author Davide Bellettini <davide.bellettini@gmail.com>
- */
-
-/**
- * Base DB class.
- */
 abstract class ActiveRecord
 {
     public function __construct(protected readonly Connection $dbalConnection)

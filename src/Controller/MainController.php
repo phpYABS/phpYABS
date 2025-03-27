814ace3539440571aa1dda2469ece1b3f801ebf3
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-03-27 20:57:35 +0100

    Remove homepage, redirect to purchase

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PhpYabs\Controller;
 
-use PhpYabs\Configuration\Constants;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,14 +11,9 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/')]
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('')]
     public function index(): Response
     {
-        global $intestazione;
-
-        return $this->render('index.html.twig', [
-            'version' => Constants::VERSION,
-            'header' => $intestazione,
-        ]);
+        return $this->redirectToRoute('purchase_current', ['id' => 'current']);
     }
 }

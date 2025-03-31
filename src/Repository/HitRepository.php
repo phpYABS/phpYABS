daefe676d1faaab1e7d47446642df21887c2dc47
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-04-01 01:41:38 +0200

    Remove dead comments

--- src/Repository/HitRepository.php
+++ src/Repository/HitRepository.php
@@ -22,29 +22,4 @@ class HitRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Hit::class);
     }
-
-    //    /**
-    //     * @return Hits[] Returns an array of Hits objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('a.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Hits
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }

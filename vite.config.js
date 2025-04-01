c42170d59ed2451ad012d8169ff87e422fa5ada1
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-04-01 21:47:59 +0200

    More boilerplate

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import symfonyPlugin from "vite-plugin-symfony";
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
         symfonyPlugin({
             viteDevServerHostname: "localhost",
         }),
+        vue(),
     ],
     build: {
         rollupOptions: {
